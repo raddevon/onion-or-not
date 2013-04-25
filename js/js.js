c8f1a62a267efad87716a511a61ea00c28ca0db2
Luiz <luiz.neves7@gmail.com>
2013-04-24 19:00:46 -0700

    fix white box and first animation

--- js/js.js
+++ js/js.js
@@ -3,12 +3,21 @@
 $(document).ready(function() {
 
 // $full = "imgs/Fullgreen.jpg";
+$width = $(".social").width();
+$source = $('#source');
+console.log($width);
+
+// $source.width($width);
 
 
 
 	$("#onion").on("click",function(){
-		$("#question").css("display", "none");
-		$("#Not").css("display", "inline-block");
+		$("#question").animate({
+			bottom: '-500'
+		}, 1500);
+		$("#response").animate({
+			top: '0'
+		}, 2000);
 		$("body").css("background", "url('imgs/Fullgreen.jpg')");
 		$("body").css("background-size", "cover");
 
@@ -16,16 +25,16 @@ $(document).ready(function() {
 
 	$("#next").on("click",function(){
 		$("#question").css("display", "inline-block");
-		$("#Not").css("display","none");
+		$("#response").css("display","none");
 		$("body").css("background", "url('imgs/halfgreen.jpg')");
 		$("body").css("background-size", "cover");
 
 	
 	});
 
-		$("#not").on("click",function(){
+		$("#response").on("click",function(){
 			$("#question").css("display", "none");
-			$("#Not").css("display", "inline-block");
+			$("#response").css("display", "inline-block");
 			$("body").css("background", "url('imgs/nogreen.jpg')");
 			$("body").css("background-size", "cover");
 
