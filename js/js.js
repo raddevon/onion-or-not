fcfdd9b3f2a8ba041f36b9c70c34f30c2ce3ec24
Luiz <luiz.neves7@gmail.com>
2013-05-01 18:06:30 -0700

    start with css transitions

--- js/js.js
+++ js/js.js
@@ -1,42 +1,20 @@
-
-
 $(document).ready(function() {
 
 // $full = "imgs/Fullgreen.jpg";
 $width = $(".social").width();
 $source = $('#source');
+$onion = $("#onion");
+
 console.log($width);
 
-// $source.width($width);
+// $('#response').css('display', 'none');
 
+$onion.on("click",function(){
+	$("#question").addClass("active");
 
-
-	$("#onion").on("click",function(){
-		$("#question").animate({
-			bottom: '-500'
-		}, 1500);
-		$("#response").animate({
-			top: '0'
-		}, 2000);
-		$("body").css("background", "url('imgs/Fullgreen.jpg')");
-		$("body").css("background-size", "cover");
-
-	});
-
-	$("#next").on("click",function(){
-		$("#question").css("display", "inline-block");
-		$("#response").css("display","none");
-		$("body").css("background", "url('imgs/halfgreen.jpg')");
-		$("body").css("background-size", "cover");
-
-	
-	});
-
-		$("#response").on("click",function(){
-			$("#question").css("display", "none");
-			$("#response").css("display", "inline-block");
-			$("body").css("background", "url('imgs/nogreen.jpg')");
-			$("body").css("background-size", "cover");
-
-	});
 });
+
+
+
+});
+
