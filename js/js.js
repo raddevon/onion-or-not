5f157ac9293a87b86e5ebb6f07ff959a88ee4eb5
Luiz <luiz.neves7@gmail.com>
2013-05-06 07:36:39 -0700

    add json file and default social media buttons

--- js/js.js
+++ js/js.js
@@ -1,20 +1,99 @@
-$(document).ready(function() {
 
-// $full = "imgs/Fullgreen.jpg";
-$width = $(".social").width();
+
+$Qheight = $('#question').outerHeight() + 'px';
+$Rheight = $('#response').outerHeight()+ 'px';
+$Theight = $('#question').outerHeight() + $('#response').outerHeight()+ 'px';
 $source = $('#source');
 $onion = $("#onion");
+$not = $("#not");
+$next = $("#next");
+SetA = [];
+var item;
 
-console.log($width);
 
-// $('#response').css('display', 'none');
+
+
+$(document).ready(function() {
+
+$default = function() {
+	less.modifyVars({
+	'@Rheight': $Rheight,
+	'@Qheight': $Qheight,
+	'@Theight': $Theight
+	});
+};
+// $.getScript('js/less.js');
+
+// $full = "imgs/Fullgreen.jpg";
+
+$default();
+
+// 	$.getJSON('js/links.json', function(data) {
+
+// 		SetA = data.Headlines.slice(0);
+// 		item = SetA[Math.floor(Math.random()*SetA.length)];
+
+
+// 	$back =	$( "<h1/>", {
+//   		"id": "headline",
+//   		"text": item.headline
+// });
+	// alert($back.height);
+
+});
 
 $onion.on("click",function(){
-	$("#question").addClass("active");
+	$("#response").addClass("answer");
+	$("#question").addClass("answer");
+	$("#white").addClass("answer");
 
+});
+
+$not.on("click",function(){
+	$("#response").addClass("answer");
+	$("#question").addClass("answer");
+	$("#white").addClass("answer");
 });
 
 
 
-});
+$next.on("click", function() {
+
+
+
+	$.getJSON('js/links.json', function(data) {
+		// SetA = data.Headlines.slice(0);
+		if (SetA.length !== 0) {
+			var item = SetA[Math.floor(Math.random()*SetA.length)];
+			SetA.splice(item,1);
+				console.log(item.url);
+				// $url = 
+				$('#headline').replaceWith('<h1 id = "headline">' + item.headline + '</h1>');
+				// $('#headline').animate;
+
+				$('#source').replaceWith("<a target='_blank' id='source' href = " + item.url + ">link</a>");
+			}
+
+		else {
+		SetA = data.Headlines.slice(0);
+				var item = SetA[Math.floor(Math.random()*SetA.length)];
+				$('#headline').replaceWith('<h1 id = "headline">' + item.headline + '</h1>');
+				$('#source').replaceWith("<a target='_blank' id='source' href = " + item.url + "></a>");
+		console.log(SetA);
+	}
+
+		$("#response").removeClass("answer");
+		$("#question").removeClass("answer");
+		$("#white").removeClass("answer");
+
+
+
+		// Headlines[0].headline
+  });
+
+	});
+// });
+
+
+
 
