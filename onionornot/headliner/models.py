a0cb802f915a5e73e289ed4ef20359d50b5e7038
Devon Campbell <raddevon@gmail.com>
2013-07-03 21:57:12 -0400

    Cleanup

--- onionornot/headliner/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Headline(models.Model):
-    text = models.CharField(verbose_name="headline text", unique=True)
-    source = models.URLField(verbose_name="URL", unique=True)
